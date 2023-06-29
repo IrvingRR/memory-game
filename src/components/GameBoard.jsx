f826c3cd210b47594eb1fdde0fdb8aeac70e481a
Irving Rodriguez <irvingrodriguezromero@gmail.com>
2023-06-29 11:47:01 -0600

    [Feature]: Random order functionality created

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { GameBoardElement } from '../styled/components/GameBoard.styles';
 import { GameCard } from './GameCard';
 import MoonSvg from '../assets/moon.svg';
@@ -5,17 +6,33 @@ import StarSvg from '../assets/star.svg';
 import SunSvg from '../assets/sun.svg';
 import CometSvg from '../assets/comet.svg';
 
+const cardImages = [
+  { src: MoonSvg },
+  { src: StarSvg },
+  { src: SunSvg },
+  { src: CometSvg },
+];
+
 export const GameBoard = () => {
+
+  const [cards, setCards] = useState([]);
+
+  const generateRandomOrder = () => {
+    const randomOrder = [...cardImages, ...cardImages]
+    randomOrder.sort(() => Math.random() - 0.5);
+
+    const shuffleCards = randomOrder.map(cardImage => ({ ...cardImage, id: Math.random() * 999 }));
+    setCards(shuffleCards);
+  };
+
+  useEffect(() => {
+    generateRandomOrder();
+  }, []);
+
+
   return (
     <GameBoardElement>
-        <GameCard picture={MoonSvg}/>
-        <GameCard picture={StarSvg}/>
-        <GameCard picture={SunSvg}/>
-        <GameCard picture={CometSvg}/>
-        <GameCard picture={MoonSvg}/>
-        <GameCard picture={StarSvg}/>
-        <GameCard picture={SunSvg}/>
-        <GameCard picture={CometSvg}/>
+      { cards.map(card => <GameCard picture={card.src} key={card.id}/>) }
     </GameBoardElement>
   );
 };
