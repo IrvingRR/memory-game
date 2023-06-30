2de2e04496ff30a7744faadb4e1888ddbd39207a
Irving Rodriguez <irvingrodriguezromero@gmail.com>
2023-06-30 00:06:27 -0600

    [Changes]: Text changed in GameUnsolved component

--- src/components/GameUnsolved.jsx
+++ src/components/GameUnsolved.jsx
@@ -2,7 +2,7 @@ export const GameUnsolved = () => {
     return (
       <>
           <h2>
-            Oops you didn’t find them all<span>!</span>
+            Oops you didn’t find them all
           </h2>
           <p>You can do it, try again.</p>
       </>
