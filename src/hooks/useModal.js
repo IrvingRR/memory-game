a2d30f78af8dc5dd592cab0064c0a226f8f17b86
Irving Rodriguez <irvingrodriguezromero@gmail.com>
2023-06-30 01:19:00 -0600

    [Documentation]: Documentation of useModal hook created

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,13 +1,20 @@
 import { useState } from 'react';
 
+/* 
+    This hook allows to handle the functionality of a modal component
+*/
+
 export const useModal = (initialState = false) => {
 
     const [isModalActivated, setIsModalActivated] = useState(initialState);
 
+    // This function allows to active the modal to show it ih the application
     const activateModal = () => setIsModalActivated(true);
 
+    // This function allows to desactive the modal to hidde it in the application
     const desactivateModal = () => setIsModalActivated(false);
 
+    // This function allows to desactivate and activate the modal using the same element as a toggle
     const toggleModal = () => setIsModalActivated(!isModalActivated);
 
     return {
